16ac89f662af911a8bf681316f370b3b0ba06853
Eleni Palmos <gkbluestocking@gmail.com>
2015-07-25 20:14:02 -0400

    resubmit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,66 +1,19 @@
 var bio = {
-    "name": "Eleni Palmos",
-    "role": "Web Developer",
-    "contacts": {
-        "mobile": "+1.718.570.3200",
-        "email": "gkbluestocking@gmail.com",
-        "github": "gkbluestocking",
-        "twitter": "@gkbluestocking",
-        "location": "New York"
+    name: "Eleni Palmos",
+    role: "About to be a Great Web Developer",
+    contacts: {
+        mobile: "+1.718.570.3200",
+        email: "gkbluestocking@gmail.com",
+        github: "gkbluestocking",
+        twitter: "@gkbluestocking",
+        location: "New York"
     },
-    "welcomeMessage": "Welcome to My Page.",
-    "bioPic": "images/eleni.jpg",
-    "skills": ["Javascript", "CSS", "HTML", "DoubleClick", "Sizmek", "Programmatic", "Excel"]
+    welcomeMessage: "Welcome to my second project for my Udacity Front End Web Developer Nanodegree.",
+    bioPic: "images/eleni.jpg",
+    skills: ["Online Advertising", "HTML CSS", "Javascript", "DoubleClick", "Sizmek", "Programmatic", "Excel"]
 };
 
-displayBio();
-
-var education = {
-    "schools": [{
-        "name": "New York University",
-        "location": "New York, NY",
-        "degree": "BA",
-        "major": ["Psychology", "Religious Studies"],
-        "dates": "1993-1997"
-    }],
-    "onlineCourses": [{
-        "title": "Front End Web Development",
-        "school": "Udacity",
-        "dates": 2015,
-        "url": "http://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-    }]
-};
-
-displayEducation();
-
-var work = {
-    "jobs": [{
-        "employer": "LiveIntent",
-        "title": "Billing Manager",
-        "dates": "October 2013 - Present",
-        "description": "Billing"
-    }, {
-        "employer": "MediaMind",
-        "title": "Billing Specialist",
-        "dates": "July 2012 - October 2013",
-        "description": "Billing"
-    }]
-};
-
-displayWork();
-
-var projects = {
-    "projects": [{
-        "title": "Sample Project 1",
-        "dates": "2015",
-        "description": "Online Resume"
-    }]
-};
-
-displayProjects();
-
-
-function displayBio() {
+bio.display = function() {
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
     var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
@@ -78,7 +31,7 @@ function displayBio() {
     $("#topContacts").append(formattedGithub);
     $("#topContacts").append(formattedTwitter);
     $('#footerContacts').append(formattedMobile);
-}
+};
 
 if (bio.skills.length > 0) {
 
@@ -89,62 +42,84 @@ if (bio.skills.length > 0) {
 
     bio.skills.forEach(function(skill) {
         formattedSkill = HTMLskills.replace("%data%", skill);
-        console.log(formattedSkill);
         $skillList.append(formattedSkill);
     });
 }
 
+bio.display();
 
-function displayEducation() {
-    for (school in education.schools) {
-        $("#education").append(HTMLschoolStart);
-
-        var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-        var formattedSchoolHeader = formattedName + formattedDegree;
-
-        $("#education").append(HTMLschoolStart);
-        $(".education-entry:last").append(formattedSchoolHeader);
-        $(".education-entry:last").append(formattedDates);
-
-        if (education.schools[school].major.length > 0) {
-            for (item in education.schools[school].major)
-                var schoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
-            $(".school-entry:last").append(schoolMajor);
-        }
+var education = {
+    school: {
+        name: "New York University",
+        location: "New York, NY",
+        degree: "Bachelor of the Arts",
+        major: "Psychology with a focus on learning and conditioning",
+        date: "1997"
     }
 }
 
-function displayWork() {
-    for (job in work.jobs) {
-        $("#workExperience").append(HTMLworkStart);
+education.display = function() {
+    var formattedName = HTMLschoolName.replace("%data%", education.school.name);
+    var formattedDegree = HTMLschoolDegree.replace("%data%", education.school.degree);
+    var formattedMajor = HTMLschoolMajor.replace("%data%", education.school.major);
+    var formattedDate = HTMLschoolDates.replace("%data%", education.school.date);
+    var formattedSchoolHeader = formattedName + formattedDegree + formattedDate;
+    $("#education").append(HTMLschoolStart);
+    $(".education-entry:last").append(formattedSchoolHeader);
+    $(".education-entry:last").append(formattedMajor);
+};
 
+education.display();
+
+var work = {
+    jobs: [{
+        employer: "LiveIntent",
+        title: "Billing Manager",
+        dates: "October 2013 - Present",
+        description: "Billing"
+    }, {
+        employer: "MediaMind",
+        title: "Billing Specialist",
+        dates: "July 2012 - October 2013",
+        description: "Billing"
+    }]
+};
+
+work.display = function() {
+    for (job in work.jobs) {
         var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
         var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
         var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
-        $(".work-entry:last").append(formattedEmployerTitle);
-
         var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-        $(".work-entry:last").append(formattedDates);
-
         var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $("#workExperience").append(HTMLworkStart);
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(formattedDates);
         $(".work-entry:last").append(formattedDescription);
     }
-}
+};
 
-function displayProjects() {
+work.display();
+
+var projects = {
+    projects: [{
+        title: "Project One: My Online Resume",
+        dates: "2015",
+        description: "The very resume you are reading here."
+    }]
+};
+
+projects.display = function() {
     for (project in projects.projects) {
-        $("#projects").append(HTMLprojectStart);
-
         var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-        $(".project-entry:last").append(formattedTitle);
-
         var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-        $(".project-entry:last").append(formattedDates);
-
         var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+
+        $("#projects").append(HTMLprojectStart);
+        $(".project-entry:last").append(formattedTitle);
+        $(".project-entry:last").append(formattedDates);
         $(".project-entry:last").append(formattedDescription);
     }
-}
+};
+
+projects.display();
